56df40f224fb072c541356e4b60e5eda46fa6f09
unknown <urs.lang@gmail.com>
2020-12-16 14:11:23 +0100

    .

--- src/App/UserAction/Form/UserChangeForm.php
+++ src/App/UserAction/Form/UserChangeForm.php
@@ -4,12 +4,11 @@ namespace Nemundo\App\UserAction\Form;
 
 
 use Nemundo\Com\FormBuilder\AbstractFormBuilder;
+use Nemundo\Core\Http\Url\UrlReferer;
+use Nemundo\User\Builder\UserBuilder;
 use Nemundo\User\Com\ListBox\UserListBox;
 use Nemundo\User\Login\UserLogin;
 use Nemundo\User\Session\UserSession;
-use Nemundo\User\Type\UserItemType;
-use Nemundo\User\Type\UserSessionType;
-use Nemundo\Core\Http\Url\UrlReferer;
 
 class UserChangeForm extends AbstractFormBuilder
 {
@@ -22,10 +21,9 @@ class UserChangeForm extends AbstractFormBuilder
     public function getContent()
     {
 
-        $this->userListBox =new UserListBox($this);
+        $this->userListBox = new UserListBox($this);
         $this->userListBox->submitOnChange = true;
         $this->userListBox->emptyValueAsDefault = false;
-        //$this->userListBox->filter->andEqual($this->userListBox->model->active, true);
         $this->userListBox->value = (new UserSession())->userId;
 
         return parent::getContent();
@@ -36,11 +34,11 @@ class UserChangeForm extends AbstractFormBuilder
     protected function onSubmit()
     {
 
-        $userType = new UserItemType($this->userListBox->getValue());
+        $userRow = (new UserBuilder($this->userListBox->getValue()))->getUserRow();
 
         $userLogin = new UserLogin();
-        $userLogin->login = $userType->login;
-        $userLogin->password = $userType->login;
+        $userLogin->login = $userRow->login;
+        $userLogin->password = $userRow->login;
         $userLogin->passwordVerification = false;
         $userLogin->loginUser();
 
