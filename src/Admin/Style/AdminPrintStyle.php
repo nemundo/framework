57aa6c8378981bcfce8a9233c91cd83706fe0b0c
Urs Lang <urs.lang@gmail.com>
2022-09-21 22:10:22 +0200

    .

---
+++ src/Admin/Style/AdminPrintStyle.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Nemundo\Admin\Style;
+
+class AdminPrintStyle
+{
+
+    public static $FONTSIZE_6 = 'admin-fontsize-6';
+
+    public static $FONTSIZE_8 = 'admin-fontsize-8';
+
+    public static $FONTSIZE_10 = 'admin-fontsize-10';
+
+    public static $FONTSIZE_12 = 'admin-fontsize-12';
+
+    public static $FONTSIZE_14 = 'admin-fontsize-14';
+
+
+
+}
