966095b7c4aff1e71dd881ad6bb1d6eaa530e8b6
Urs Lang <urs.lang@gmail.com>
2022-10-06 11:09:20 +0200

    .

---
+++ src/Admin/Site/AbstractKmlSite.php
@@ -0,0 +1,1 @@
+<?phpnamespace Nemundo\Admin\Site;use Nemundo\Web\Site\AbstractSiteTree;abstract class AbstractKmlSite extends AbstractAdminIconSite{    public function __construct(AbstractSiteTree $site = null)    {        $this->title = 'Kml';        $this->url = 'kml';        parent::__construct($site);        $this->iconName = 'globe';    }}
