5633a48d86620dd39569f3acc792266846134ef9
Urs Lang <urs.lang@gmail.com>
2023-04-22 20:12:49 +0200

    .

--- bin/test.php
+++ bin/test.php
@@ -1,20 +1,4 @@
 <?php
 
-use Nemundo\Office\Excel\Reader\ExcelReader;
-
 require "config.php";
 
-$filename = 'D:\Tmp_Git\regiocockpit\tmp\bauzonenstatistik.xlsx';
-
-$reader = new ExcelReader();
-//$reader->setSheet('asdf');
-$reader->filename = $filename;
-
-//(new \Nemundo\Core\Debug\Debug())->write($reader->getFileFormat());
-
-foreach ($reader->getData() as $excelRow) {
-
-    /*$excelRow->hasValueByNumber(0);
-    $excelRow->getValueByNumber(0);*/
-
-}
