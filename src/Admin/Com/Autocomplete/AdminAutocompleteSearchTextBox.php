32f55bdd627a6cb9f60244679ebb0c326d386484
Lang Urs <urs.lang@luzern.com>
2024-01-17 19:08:43 +0100

    .

--- src/Admin/Com/Autocomplete/AdminAutocompleteSearchTextBox.php
+++ src/Admin/Com/Autocomplete/AdminAutocompleteSearchTextBox.php
@@ -4,6 +4,7 @@ namespace Nemundo\Admin\Com\Autocomplete;
 
 use Nemundo\Admin\Com\ListBox\AdminIconTextBox;
 use Nemundo\App\WebService\Service\AbstractWordWebService;
+use Nemundo\Core\Http\Request\HttpRequest;
 use Nemundo\Html\Block\Div;
 use Nemundo\Html\Form\Input\HiddenInput;
 use Nemundo\Html\Script\ModuleJavaScript;
@@ -24,8 +25,6 @@ class AdminAutocompleteSearchTextBox extends AdminIconTextBox  // AdminSearchTex
     public function getContent()
     {
 
-        $hiddenName = $this->name . '_id';
-
         $this->icon = 'search';
 
         $module = new ModuleJavaScript($this);
@@ -33,38 +32,46 @@ class AdminAutocompleteSearchTextBox extends AdminIconTextBox  // AdminSearchTex
             ->addContent('import Debug from "/package/js/core/Debug/Debug.js";')
             ->addContent('import DocumentContainer from "/package/js/html/Document/Document.js";')
             ->addContent('import AutocompleteInput from "/package/js/framework/Com/Autocomplete/AutocompleteInput.js";')
+            ->addContent('import HiddenInputContainer from "/package/js/html/Form/HiddenInput.js";')
             ->addContent('let document = new DocumentContainer();')
             ->addContent('document.onLoaded = function () {')
             ->addContent('let autocomplete = new AutocompleteInput();')
             ->addContent('autocomplete.serviceName = "' . $this->webService->serviceName . '";')
             ->addContent('autocomplete.fromId("' . $this->name . '");')
             ->addContent('autocomplete.loadAutocomplete();')
-            ->addContent('autocomplete.onWordChange() {')
+            ->addContent('autocomplete.onWordChange = function(id) {')
             ->addContent('let hidden = new HiddenInputContainer();')
-            ->addContent('hidden.fromId("' . $hiddenName . '");')
-            ->addContent('hidden.value = search.value;')
-            ->addContent('}')
+            ->addContent('hidden.fromId("' . $this->getHiddenName() . '");')
+            ->addContent('hidden.value = id;')
+            ->addContent('};')
             ->addContent('};');
-
 
         $wordList = new Div($this);
         $wordList->id = $this->webService->serviceName . '_word_list';
 
         $this->hidden = new HiddenInput($this);
-        $this->hidden->name = $hiddenName;
-        $this->hidden->id = $hiddenName;
+        $this->hidden->name = $this->getHiddenName();
+        $this->hidden->id = $this->getHiddenName();
 
         return parent::getContent();
 
     }
 
-
     public function getId()
     {
 
-        return $this->hidden->value;
+        $request = new HttpRequest($this->getHiddenName());
+        return $request->getValue();
 
     }
 
+
+    private function getHiddenName()
+    {
+
+        $name = $this->name . '_id';
+        return $name;
+
+    }
 
 }
