2e5ae055cc593799db772bedfff8c18bc5720cea
Lang Urs <urs.lang@luzern.com>
2024-03-09 10:42:27 +0100

    .

--- src/Admin/Com/Item/AdminItemSiteHyperlinkContainer.php
+++ src/Admin/Com/Item/AdminItemSiteHyperlinkContainer.php
@@ -4,13 +4,14 @@ namespace Nemundo\Admin\Com\Item;
 
 use Nemundo\Com\Html\Hyperlink\SiteHyperlink;
 
-class AdminItemHyperlinkContainer extends SiteHyperlink
+class AdminItemSiteHyperlinkContainer extends SiteHyperlink
 {
 
     public function getContent()
     {
 
         $this->addCssClass('admin-item-container');
+        $this->addCssClass('admin-item-container-hyperlink');
         $this->addCssClass('admin-hover');
         $this->showSiteTitle = false;
         return parent::getContent();
--- src/Admin/Com/Item/AdminItemUrlHyperlinkContainer.php
+++ src/Admin/Com/Item/AdminItemUrlHyperlinkContainer.php
@@ -4,13 +4,17 @@ namespace Nemundo\Admin\Com\Item;
 
 use Nemundo\Com\Html\Hyperlink\UrlHyperlink;
 
-class AdminItemUrlHyperlink extends UrlHyperlink
+class AdminItemUrlHyperlinkContainer extends UrlHyperlink
 {
 
     public function getContent()
     {
 
+
+        $this->addCssClass('admin-item-container');
         $this->addCssClass('admin-item-hyperlink');
+        $this->addCssClass('admin-item-container-hyperlink');
+        $this->addCssClass('admin-hover');
 
         return parent::getContent();
     }
