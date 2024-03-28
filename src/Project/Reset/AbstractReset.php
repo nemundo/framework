fe075d12d640368c638dd89fd16f576d22a94483
Lang Urs <urs.lang@luzern.com>
2024-03-28 16:35:53 +0100

    .

--- src/Project/Reset/AbstractReset.php
+++ src/Project/Reset/AbstractReset.php
@@ -1,1 +1,1 @@
-<?phpnamespace Nemundo\Project\Reset;use Nemundo\Core\Base\AbstractBase;abstract class AbstractReset extends AbstractBase{    abstract public function remove();}
+<?phpnamespace Nemundo\Project\Reset;use Nemundo\Core\Base\AbstractBase;abstract class AbstractReset extends AbstractBase{    abstract public function reset();    abstract public function remove();}
