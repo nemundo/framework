6bb7426fdb9c81d1810cf68ed9c797bb0e2720ad
Lang Urs <urs.lang@luzern.com>
2025-06-10 13:49:59 +0200

    .

--- src/Admin/Com/HtmlEditor/AdminHtmlEditor.php
+++ src/Admin/Com/HtmlEditor/AdminHtmlEditor.php
@@ -3,17 +3,49 @@
 namespace Nemundo\Admin\Com\HtmlEditor;
 
 use Nemundo\Admin\Com\ListBox\AdminLargeTextBox;
-use Nemundo\Package\CkEditor5\CkEditor5Trait;
+use Nemundo\Html\Header\Link\StylesheetLink;
+use Nemundo\Html\Script\JavaScript;
 
 class AdminHtmlEditor extends AdminLargeTextBox
 {
 
-    use CkEditor5Trait;
+    //use CkEditor5Trait;
 
     public function getContent()
     {
 
-        $this->loadCkEditor($this->name);
+        //$this->loadCkEditor($this->name);
+
+
+        $stylesheet = new StylesheetLink($this);
+        $stylesheet->href = 'https://cdn.jsdelivr.net/npm/summernote@0.9.0/dist/summernote-lite.min.css';
+
+        $script = new JavaScript($this);
+        $script->src = 'https://code.jquery.com/jquery-3.4.1.slim.min.js';
+
+        $script = new JavaScript($this);
+        $script->src = 'https://cdn.jsdelivr.net/npm/summernote@0.9.0/dist/summernote-lite.min.js';
+
+        $script = new JavaScript($this);
+        $script->addCodeLine("
+        $( document ).ready(function() {
+    $('#" . $this->name . "').summernote({
+        placeholder: '',
+        tabsize: 2,
+        height: 120,
+        toolbar: [
+        ['font', ['bold', 'underline', 'clear']],
+        ['color', ['color']],
+        ['para', ['ul', 'ol', 'paragraph']],
+        ['table', ['table']],
+        ['insert', ['link', 'picture', 'video']],
+        ['view', ['fullscreen', 'codeview', 'help']]
+    ]
+    });
+    });
+    ");
+
+
         return parent::getContent();
 
     }
