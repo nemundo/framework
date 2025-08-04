523a043309bab589af3526df1369ddc0c53018d7
Lang Urs <urs.lang@luzern.com>
2025-08-04 13:52:17 +0200

    .

--- bin/test.php
+++ bin/test.php
@@ -3,4 +3,16 @@
 require 'config.php';
 
 
+$model = new \Nemundo\App\Scheduler\Data\Scheduler\SchedulerModel();
+
+$setup = new \Nemundo\Model\Setup\ModelSetup();
+$setup->model = $model;
+$setup->createTable();
+
+
+
+
+
+
+
 
