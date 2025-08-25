f2bd52b758b98a1b6e44327d74c4d0d042da56fd
Lang Urs <urs.lang@luzern.com>
2025-08-25 10:32:21 +0200

    .

--- test/Office/Excel/ExcelDocument.php
+++ test/Office/Excel/ExcelDocument.php
@@ -5,6 +5,11 @@ require __DIR__ . '/../../config.php';
 
 $excel = new \Nemundo\Office\Excel\Document\ExcelDocument();
 $excel->filename = 'test.xlsx';   //(new \Nemundo\Project\Path\TmpPath())->addPath('test.xlsx')->getFullFilename();
+$excel->overwriteExistingFile = true;
+
+$excel->renameActiveSheet('Bla0');
+
+$excel->addSheet('bla1');
 
 $data= [];
 $data[]='Column1';
@@ -14,7 +19,14 @@ $excel->addBoldRow($data);
 $excel->addRow($data);
 
 
+$excel->addSheet('bla2');
 
+$data= [];
+$data[]='Column1';
+$data[]='Column2 Column2 Column2 Column2 Column2 ';
+
+$excel->addBoldRow($data);
+$excel->addRow($data);
 
 
 $excel->writeFile((new \Nemundo\Project\Path\TmpPath())->getFullFilename());
