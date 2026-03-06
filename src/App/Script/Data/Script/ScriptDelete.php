1ad3b49bf4a0213d7a10bdf514a65bd75db7e14f
urs-lang <urs.lang@nemundo.ch>
2026-03-06 11:09:45 +0100

    .

--- src/App/Script/Data/Script/ScriptDelete.php
+++ src/App/Script/Data/Script/ScriptDelete.php
@@ -1,1 +1,13 @@
-<?phpnamespace Nemundo\App\Script\Data\Script;class ScriptDelete extends \Nemundo\Model\Delete\AbstractModelDelete {/*** @var ScriptModel*/public $model;public function __construct() {parent::__construct();$this->model = new ScriptModel();}}
+<?php
+namespace Nemundo\App\Script\Data\Script;
+class ScriptDelete extends \Nemundo\Model\Delete\AbstractModelDelete {
+/**
+* @var ScriptModel
+*/
+public $model;
+
+public function __construct() {
+parent::__construct();
+$this->model = new ScriptModel();
+}
+}
